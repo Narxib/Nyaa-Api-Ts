2d9e29918ca47d84a329d3d16658b75c221643c3
Brian Zaragoza Cerdá <brianzaragoza25@gmail.com>
2025-01-15 11:56:23 +0100

    test

--- src/routes.ts
+++ src/routes.ts
@@ -47,7 +47,7 @@ export class Handlers {
       const searchUrl = `${baseUrl}?q=${queryParams.query.trim()}&c=${category}&p=${
         queryParams.page
       }&s=${queryParams.sort}&o=${queryParams.order}&f=${queryParams.filter}`;
-
+      console.log(searchUrl)
       await Scrapers.scrapeNyaa(res, searchUrl);
     } catch (error) {
       res.send(404, "Not Found");
